81a234b62fb30d37dd7b2e7f133561b228bd6f7e
Hoboman <50063217+Hoboman57@users.noreply.github.com>
2025-07-24 23:48:49 +1000

    un fucked previous push

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-{
- "cells": [],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "name": "python",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv() #Loads the .env file for the fucken token
+TOKEN = os.getenv("DISCORD_TOKEN") #Get the token from the .env file
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am a robot")
+
+bot.run(TOKEN)
